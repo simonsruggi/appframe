8581667132d953a3098c5281a731cd5d6dc2dada
simonsruggi <simone.ruggiero97@gmail.com>
2026-03-17 14:39:41 +0100

    Make sitemap dynamic (force-dynamic)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { posts } from "./blog/posts";
 
+export const dynamic = "force-dynamic";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://appfra.me";
 
